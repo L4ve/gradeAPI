487e592fbe0657f3193550acddf9360f30c63982
Your Name <you@example.com>
2025-04-04 09:17:00 +0200

    21jnwwuq

--- index.js
+++ index.js
@@ -16,21 +16,39 @@ app.use(express.json()); // Middleware to parse JSON request bodies
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-app.get('/grades/:student_id/', async (req, res) => {
+app.get('/students/:student_id/grades/', async (req, res) => {
     const studentId = req.params.student_id;
     console.log("Student ID:", studentId);
     const query = 'SELECT * FROM grades WHERE student_id = ' + studentId;
     const result = await client.query(query);
     console.log("Query Result:");
     console.table(result.rows);
-    res.end();
+    res.send(result.rows);
 });
-app.post('/grades/:student_id/', async (req, res) => {
-    const studentId = req.params.student_id;
+app.post('/students/:student_id/grades/', async (req, res) => {
+    const studentId = parseInt(req.params.student_id);
     console.log("Student ID:", studentId);
     console.log("Request Body:", req.body);
+
+    if(studentId === null || !Number.isInteger(studentId) || !isFinite(studentId)) {
+      res.status(400).send('Invalid student ID');
+      return;
+    }
+    if(req.body.course_id === null || !Number.isInteger(req.body.course_id) || !isFinite(req.body.course_id)) {
+      res.status(400).send('Invalid student ID');
+      return;
+    }
+    if(req.body.grade === null || !Number.isInteger(req.body.grade) || !isFinite(req.body.grade)) {
+      res.status(400).send('Invalid student ID');
+      return;
+    }
+
+    const query = 'INSERT INTO grades (student_id, course_id, grade) VALUES ($1, $2, $3)';
+    const values = [studentId, req.body.course_id, req.body.grade];
+    await client.query(query, values);
     res.end();
 });
+
 app.listen(3000, async () => {
     await client.connect(); // Connect to the database
     console.log('Connected to the database');
